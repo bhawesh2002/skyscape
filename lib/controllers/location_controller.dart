846c7514588e54a6b75b9f206b5686ff594251ca
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-27 10:56:12 +0530

    create LocationController that creates an instance of GetLocation to fetch current coordinates

---
+++ lib/controllers/location_controller.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_weather_app/api/get_location.dart';
+
+class LocationController extends GetxController {
+  RxDouble lat = 0.0.obs;
+  RxDouble long = 0.0.obs;
+  final GetLocation _myLocation = GetLocation();
+  Future<void> setLocation() async {
+    try {
+      final serviceEnabled = await _myLocation.isServiceEnabled();
+      final permissionGranted = await _myLocation.isPermissionGranted();
+      if (serviceEnabled && permissionGranted) {
+        final locationData = await _myLocation.getLocation();
+        lat.value = locationData.latitude!;
+        long.value = locationData.longitude!;
+      }
+      update();
+    } catch (e) {
+      debugPrint("Error has Occured wile setting location: $e");
+    }
+  }
+}
