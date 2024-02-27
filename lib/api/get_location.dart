ec34a52be6bacabdba3643d6d63b569c448291dc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-27 10:54:53 +0530

    create GetLocation class that uses location package to fetch coordinates

---
+++ lib/api/get_location.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class GetLocation {
+  final Location _location = Location();
+
+  Future<bool> isServiceEnabled() async {
+    bool serviceChecker = await _location.serviceEnabled();
+    if (serviceChecker) {
+      debugPrint("Service Status: ${serviceChecker.toString()}");
+      return serviceChecker;
+    } else if (!serviceChecker) {
+      debugPrint("GetLocation Status: Enabling Service");
+      serviceChecker = await _location.requestService();
+      debugPrint("Service Status: ${serviceChecker.toString()}");
+      return serviceChecker;
+    }
+    return false;
+  }
+
+  Future<bool> isPermissionGranted() async {
+    PermissionStatus permissionStatus = await _location.hasPermission();
+    if (permissionStatus == PermissionStatus.granted) {
+      debugPrint("Location Permission: ${permissionStatus.toString()}");
+      return true;
+    } else if (permissionStatus == PermissionStatus.denied) {
+      debugPrint("Get Loaction Status: Requesting permission....");
+      permissionStatus = await _location.requestPermission();
+      debugPrint("Location Permission: ${permissionStatus.toString()}");
+      return true;
+    }
+    return false;
+  }
+
+  Future<LocationData> getLocation() async {
+    debugPrint("GetLocation Status: Fetching Coordinates");
+    LocationData coordinates = await _location.getLocation();
+    debugPrint("GetLocation Status: Coordinates Fetched Successfully");
+    return coordinates;
+  }
+}
