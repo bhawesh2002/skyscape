7240c955be2ea612cb421c835695ad8e7a1978d4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-27 06:46:31 +0530

    create class AppPages with List<GetPages> pages

---
+++ lib/routes/app__pages.dart
@@ -0,0 +1,27 @@
+import 'package:get/get.dart';
+import 'package:getx_weather_app/routes/app_routes.dart';
+import 'package:getx_weather_app/screen/home.dart';
+import 'package:getx_weather_app/screen/login.dart';
+import 'package:getx_weather_app/screen/settings.dart';
+import 'package:getx_weather_app/screen/signup.dart';
+
+class AppPages {
+  static final List<GetPage> pages = [
+    GetPage(
+      name: AppRoutes.login,
+      page: () => const LoginPage(),
+    ),
+    GetPage(
+      name: AppRoutes.signup,
+      page: () => const SignUpPage(),
+    ),
+    GetPage(
+      name: AppRoutes.home,
+      page: () => const HomePage(),
+    ),
+    GetPage(
+      name: AppRoutes.settings,
+      page: () => const SettingsPage(),
+    ),
+  ];
+}
