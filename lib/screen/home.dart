d317be8e8e66164e33374d9337bef1506e75fa2b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-02-27 10:57:20 +0530

    initialise LocationController and use GetBuilder to display current coordinates

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_weather_app/controllers/location_controller.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+  final LocationController _locationController = Get.put(LocationController());
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("This is Home Page"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GetBuilder<LocationController>(
+              builder: (locationController) => Text(
+                "Latitude: ${locationController.lat.value}",
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(20),
+            ),
+            GetBuilder<LocationController>(
+              builder: (locationController) => Text(
+                "Longitude: ${locationController.long.value}",
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await _locationController.setLocation();
+        },
+        child: const Icon(Icons.gps_fixed),
       ),
     );
   }
