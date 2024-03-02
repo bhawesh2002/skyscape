78a6cb7bedcc071c4f11bf01d6b40651b3d7b3ea
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-02 16:14:20 +0530

    remove unused imports

--- lib/screen/search.dart
+++ lib/screen/search.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:getx_weather_app/controllers/weater_controller.dart';
 import 'package:getx_weather_app/models/owm_city_list.dart';
