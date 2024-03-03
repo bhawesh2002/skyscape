ce16a6f88eb0b1fb6782c8a1afa820e20d983dd8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-03 11:49:50 +0530

    set initialRoute as AppRoutes.login

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx_weather_app/routes/app__pages.dart';
+import 'package:getx_weather_app/routes/app_routes.dart';
 import 'package:getx_weather_app/screen/home.dart';
-import 'package:getx_weather_app/utils/enums/theme/themes.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,11 +13,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      initialRoute: AppRoutes.login,
       getPages: AppPages.pages,
       home: HomePage(),
-      darkTheme: AppThemes.darkTheme,
-      theme: AppThemes.darkTheme,
-      themeMode: ThemeMode.dark,
     );
   }
 }
