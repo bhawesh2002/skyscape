63f1936aef7a62855204a48eede5dd5cb841f59f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-12 20:06:21 +0530

    Add SavedCity widget to display saved cities

--- lib/screen/search.dart
+++ lib/screen/search.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get/get_core/get_core.dart';
 import 'package:getx_weather_app/controllers/search_controller.dart';
 import 'package:getx_weather_app/controllers/weater_controller.dart';
 import 'package:getx_weather_app/routes/app_routes.dart';
 import 'package:getx_weather_app/widgets/city_list_tile.dart';
+import 'package:getx_weather_app/widgets/saved_city.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
@@ -63,15 +65,108 @@ class SearchPage extends StatelessWidget {
             ),
             Positioned.fill(
               top: Get.height * 0.1,
+              left: Get.width * 0.05,
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  _searchController.searching.value == true
+                      ? _searchController.filterCities.isEmpty
+                          ? "No City Found"
+                          : "Searching"
+                      : "Saved Cities",
+                  style: GoogleFonts.montserrat(
+                      fontSize: Get.width * 0.05,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.black),
+                ),
+              ),
+            ),
+            Positioned.fill(
+              top: Get.height * 0.165,
               child: Align(
                 alignment: Alignment.topCenter,
                 child: _searchController.isLoading.value == false
                     ? _searchController.searching.value == false
-                        ? Center(
-                            child: Text(
-                              "No Saved Cities",
-                              style: GoogleFonts.montserrat(
-                                  fontSize: Get.width * 0.05),
+                        ? SizedBox(
+                            height: Get.height * 0.7,
+                            child: SingleChildScrollView(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      SavedCity(
+                                        cityName: "Bhandara",
+                                        countryShort: "IN",
+                                        districtNState: "Bhandara, Maharashtra",
+                                        pincode: "441904",
+                                        timeOfDay: Symbols.sunny_rounded,
+                                        weatherMood: Symbols.snowing,
+                                      ),
+                                      SavedCity(
+                                        cityName: "Pune",
+                                        countryShort: "IN",
+                                        districtNState: "Pune, Maharashtra",
+                                        pincode: "411035",
+                                        timeOfDay: Symbols.clear_night_rounded,
+                                        weatherMood: Symbols.thunderstorm,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: Get.height * 0.015,
+                                  ),
+                                  const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      SavedCity(
+                                        cityName: "Nagpur",
+                                        countryShort: "IN",
+                                        districtNState: "Nagpur, Maharashtra",
+                                        pincode: "420010",
+                                        timeOfDay: Symbols.sunny_rounded,
+                                        weatherMood: Symbols.sunny_snowing,
+                                      ),
+                                      SavedCity(
+                                        cityName: "Mumbai",
+                                        countryShort: "IN",
+                                        districtNState: "Mumbai, Maharashtra",
+                                        pincode: "420018",
+                                        timeOfDay: Symbols.clear_night_rounded,
+                                        weatherMood: Symbols.cyclone_rounded,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                    height: Get.height * 0.015,
+                                  ),
+                                  const Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      SavedCity(
+                                        cityName: "Nagpur",
+                                        countryShort: "IN",
+                                        districtNState: "Nagpur, Maharashtra",
+                                        pincode: "420010",
+                                        timeOfDay: Symbols.sunny_rounded,
+                                        weatherMood: Symbols.sunny_snowing,
+                                      ),
+                                      SavedCity(
+                                        cityName: "Mumbai",
+                                        countryShort: "IN",
+                                        districtNState: "Mumbai, Maharashtra",
+                                        pincode: "420018",
+                                        timeOfDay: Symbols.clear_night_rounded,
+                                        weatherMood: Symbols.cyclone_rounded,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           )
                         : Container(
@@ -84,43 +179,43 @@ class SearchPage extends StatelessWidget {
                               borderRadius:
                                   BorderRadius.circular(Get.width * 0.05),
                               color: Colors.white,
-                              child: _searchController.filterCities.isEmpty
-                                  ? SizedBox(
-                                      height: Get.height * 0.08,
-                                      child: Center(
-                                        child: Text(
-                                          "No cities Found",
-                                          style: GoogleFonts.montserrat(
-                                            fontSize: Get.width * 0.04,
-                                            fontWeight: FontWeight.w600,
-                                          ),
-                                        ),
-                                      ),
-                                    )
-                                  : ListView.builder(
-                                      shrinkWrap: true,
-                                      itemCount:
-                                          _searchController.filterCities.length,
-                                      itemBuilder: (context, index) {
-                                        return CityListTile(
-                                          onTap: () {
-                                            _weatherController
-                                                .getWeatherFromCity(
-                                                    cityName: _searchController
-                                                        .filterCities[index]
-                                                        .cityName);
-                                            _searchController.search(query: "");
-                                            Get.toNamed(AppRoutes.home);
-                                          },
+                              child:
+                                  // _searchController.filterCities.isEmpty
+                                  //     ? SizedBox(
+                                  //         height: Get.height * 0.08,
+                                  //         child: Center(
+                                  //           child: Text(
+                                  //             "No cities Found",
+                                  //             style: GoogleFonts.montserrat(
+                                  //               fontSize: Get.width * 0.04,
+                                  //               fontWeight: FontWeight.w600,
+                                  //             ),
+                                  //           ),
+                                  //         ),
+                                  //       )
+                                  //     :
+                                  ListView.builder(
+                                shrinkWrap: true,
+                                itemCount:
+                                    _searchController.filterCities.length,
+                                itemBuilder: (context, index) {
+                                  return CityListTile(
+                                    onTap: () {
+                                      _weatherController.getWeatherFromCity(
                                           cityName: _searchController
-                                              .filterCities[index].cityName,
-                                          moreDetails:
-                                              "${_searchController.filterCities[index].district}, ${_searchController.filterCities[index].state}",
-                                          countryShort: _searchController
-                                              .filterCities[index].countryShort,
-                                        );
-                                      },
-                                    ),
+                                              .filterCities[index].cityName);
+                                      _searchController.search(query: "");
+                                      Get.toNamed(AppRoutes.home);
+                                    },
+                                    cityName: _searchController
+                                        .filterCities[index].cityName,
+                                    moreDetails:
+                                        "${_searchController.filterCities[index].district}, ${_searchController.filterCities[index].state}",
+                                    countryShort: _searchController
+                                        .filterCities[index].countryShort,
+                                  );
+                                },
+                              ),
                             ),
                           )
                     : Center(
---
+++ lib/widgets/saved_city.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_weather_app/controllers/weater_controller.dart';
+import 'package:getx_weather_app/routes/app_routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SavedCity extends StatelessWidget {
+  final String imgPath;
+  final String cityName;
+  final String districtNState;
+  final String pincode;
+  final String countryShort;
+  final IconData timeOfDay;
+  final IconData weatherMood;
+  const SavedCity({
+    super.key,
+    this.imgPath = "",
+    required this.cityName,
+    required this.countryShort,
+    required this.pincode,
+    required this.districtNState,
+    required this.timeOfDay,
+    required this.weatherMood,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final WeatherController weatherController = Get.find<WeatherController>();
+    return SizedBox(
+      width: Get.width * 0.45,
+      height: Get.height * 0.25,
+      child: Material(
+        clipBehavior: Clip.antiAlias,
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(Get.width * 0.04),
+            side: const BorderSide(width: 1, color: Colors.black)),
+        child: InkWell(
+          onTap: () async {
+            await weatherController.getWeatherFromCity(cityName: cityName);
+            Get.toNamed(AppRoutes.home);
+          },
+          borderRadius: BorderRadius.circular(Get.width * 0.04),
+          child: LayoutBuilder(
+            builder: ((context, constraints) {
+              return Stack(
+                children: [
+                  //container holding city image
+                  SizedBox(
+                    width: constraints.maxWidth,
+                    height: constraints.maxHeight * 0.45,
+                    child: imgPath.isEmpty
+                        ? Container(
+                            color: Colors.black,
+                          )
+                        : Image.network(
+                            imgPath,
+                            fit: BoxFit.fill,
+                          ),
+                  ),
+                  //Time of Day
+                  Positioned.fill(
+                    left: constraints.maxWidth * 0.065,
+                    top: constraints.maxHeight * 0.05,
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Icon(
+                        timeOfDay,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  //shorthand for country
+                  Positioned.fill(
+                    right: constraints.maxWidth * 0.08,
+                    top: constraints.maxHeight * 0.05,
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Text(
+                        countryShort,
+                        style: GoogleFonts.montserrat(
+                          color: Colors.white,
+                          fontSize: constraints.maxWidth * 0.07,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  //name of the city
+                  Positioned.fill(
+                    bottom: constraints.maxHeight * 0.3,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: constraints.maxWidth * 0.065),
+                        child: Text(
+                          cityName,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: GoogleFonts.montserrat(
+                            color: Colors.white,
+                            fontSize: constraints.maxWidth * 0.1,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  //district,state and pincode
+                  Positioned.fill(
+                    top: constraints.maxHeight * 0.48,
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: constraints.maxWidth * 0.065),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            //district,state
+                            Text(
+                              districtNState,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
+                              style: GoogleFonts.montserrat(
+                                color: Colors.black,
+                                fontSize: constraints.maxWidth * 0.07,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            //seperation
+                            SizedBox(
+                              height: constraints.maxHeight * 0.02,
+                            ),
+                            //pincode
+                            Text(
+                              pincode,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                              style: GoogleFonts.montserrat(
+                                color: Colors.black,
+                                fontSize: constraints.maxWidth * 0.07,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  //bookmark icon
+                  Positioned.fill(
+                    left: constraints.maxWidth * 0.065,
+                    bottom: constraints.maxHeight * 0.045,
+                    child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: const Icon(
+                          Icons.bookmark,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  //Mood of weather
+                  Positioned.fill(
+                    right: constraints.maxWidth * 0.1,
+                    bottom: constraints.maxHeight * 0.08,
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Icon(
+                        weatherMood,
+                        size: constraints.maxWidth * 0.18,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }),
+          ),
+        ),
+      ),
+    );
+  }
+}
