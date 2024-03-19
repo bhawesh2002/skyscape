b04cba0a9ccb4f7838eedec8bac8f6118e1f4b4f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-03-19 10:39:26 +0530

    Add import statements for new controllers and delete existing controllers

--- lib/controllers/auth_state_controller.dart
+++ lib/controllers/auth_state_controller.dart
@@ -1,6 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_weather_app/controllers/location_controller.dart';
+import 'package:getx_weather_app/controllers/login_controller.dart';
+import 'package:getx_weather_app/controllers/saved_cities_db_controller.dart';
+import 'package:getx_weather_app/controllers/search_controller.dart';
+import 'package:getx_weather_app/controllers/temp_unit_controller.dart';
+import 'package:getx_weather_app/controllers/weater_controller.dart';
 import 'package:getx_weather_app/routes/app_routes.dart';
 
 class AuthStateController extends GetxController {
@@ -24,6 +30,7 @@ class AuthStateController extends GetxController {
           } else {
             isUserLoggedIn.value = true;
             currUser = user;
+            Get.delete<LoginController>();
             debugPrint("${user.email} is Signed In");
           }
         },
@@ -49,6 +56,11 @@ class AuthStateController extends GetxController {
     try {
       await FirebaseAuth.instance.signOut();
       isUserLoggedIn.value = false;
+      Get.delete<TempUnitController>();
+      Get.delete<WeatherController>();
+      Get.delete<LocationController>();
+      Get.delete<SavedCitiesDBController>();
+      Get.delete<SearchCityController>();
       Get.offAllNamed(AppRoutes.login);
     } on FirebaseAuthException catch (e) {
       Get.snackbar("Error has occured", e.message!);
