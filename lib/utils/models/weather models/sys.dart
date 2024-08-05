eb47e142d13066ba320a3c50985e2e17c7638a0e
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-05 20:03:02 +0530

    refactor: Update Sys class to use nullable types for optional properties

--- lib/utils/models/weather models/sys.dart
+++ lib/utils/models/weather models/sys.dart
@@ -1,9 +1,9 @@
 class Sys {
-  final int type;
-  final int id;
-  final String country;
-  final int sunrise;
-  final int sunset;
+  final int? type;
+  final int? id;
+  final String? country;
+  final int? sunrise;
+  final int? sunset;
 
   Sys(
       {required this.type,
