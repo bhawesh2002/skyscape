b9ffeea37dae929c3a150f05ea48d126c9ca39dc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-06 18:53:48 +0530

    refactor: Update CitiesListController to fetch cities list data from repository

--- lib/controllers/cities_list_controller.dart
+++ lib/controllers/cities_list_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:skyscape/utils/models/owm_city.dart';
 import 'package:skyscape/utils/repository/owm_cities_list_repo.dart';
@@ -12,6 +13,12 @@ class CitiesListController extends GetxController {
   }
 
   Future<void> getCitiesList() async {
-    owmCitiesList.value = await OwmCitiesListRepo().getOwmCitiesListData();
+    try {
+      debugPrint('CitiesListController.getCitiesList(): Fetching Cities List');
+      owmCitiesList.value = await OwmCitiesListRepo().getOwmCitiesListData();
+      debugPrint('CitiesListController.getCitiesList(): Cities List Fetched');
+    } catch (e) {
+      debugPrint('CitiesListController.getCitiesList() error: $e');
+    }
   }
 }
