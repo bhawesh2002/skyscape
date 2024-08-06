754280971ccda0b44462228d0cd43b57c4dddd88
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-06 16:57:47 +0530

    refactor: Update home page to use GetX for weather data display

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,59 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:skyscape/controllers/open_weather_controller.dart';
 import 'package:skyscape/routes/app_routes.dart';
-import 'package:skyscape/utils/models/weather%20models/open_weather.dart';
-import 'package:skyscape/utils/repository/open_weather_repo.dart';
 
 class HomePage extends StatefulWidget {
-  final double lat;
-  final double long;
-  const HomePage({super.key, required this.lat, required this.long});
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  final OpenWeatherController _openWeatherController =
+      Get.put(OpenWeatherController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: OpenWeatherRepo()
-            .getOpenWeatherData(lat: widget.lat, lon: widget.long),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                  "Error while fetching weather Data\n:${snapshot.error.toString()}"),
-            );
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            final OpenWeather openWeather = snapshot.data!;
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                      'Lat: ${openWeather.coord.latitude} , Lan: ${openWeather.coord.longitude}'),
-                  Text('Name: ${openWeather.name}'),
-                  Text('Base: ${openWeather.base}'),
-                  Text('COD: ${openWeather.cod.toString()}'),
-                  Text('DT: ${openWeather.dt.toString()}'),
-                  Text('ID: ${openWeather.id.toString()}'),
-                  Text('Time Zone: ${openWeather.timezone.toString()}'),
-                  Text('Visibility: ${openWeather.visibility.toString()}'),
-                  Text('Weather Main:${openWeather.weather[0].main}'),
-                  Text('Sys Country:${openWeather.sys?.country}'),
-                  Text('Cloud all:${openWeather.clouds?.all}'),
-                  Text('Wind speed:${openWeather.wind?.speed}'),
-                  Text('Rain 1H:${openWeather.rain?.oneHour}'),
-                  Text('Main temp:${openWeather.main.temp}'),
-                ],
-              ),
-            );
-          }
-        },
+      body: Obx(
+        () => _openWeatherController.weather.value != null
+            ? Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                        'Lat: ${_openWeatherController.weather.value?.coord.latitude} , Lan: ${_openWeatherController.weather.value?.coord.longitude}'),
+                    Text('Name: ${_openWeatherController.weather.value?.name}'),
+                    Text('Base: ${_openWeatherController.weather.value?.base}'),
+                    Text(
+                        'COD: ${_openWeatherController.weather.value?.cod.toString()}'),
+                    Text(
+                        'DT: ${_openWeatherController.weather.value?.dt.toString()}'),
+                    Text(
+                        'ID: ${_openWeatherController.weather.value?.id.toString()}'),
+                    Text(
+                        'Time Zone: ${_openWeatherController.weather.value?.timezone.toString()}'),
+                    Text(
+                        'Visibility: ${_openWeatherController.weather.value?.visibility.toString()}'),
+                    Text(
+                        'Weather Main:${_openWeatherController.weather.value?.weather[0].main}'),
+                    Text(
+                        'Sys Country:${_openWeatherController.weather.value?.sys?.country}'),
+                    Text(
+                        'Cloud all:${_openWeatherController.weather.value?.clouds?.all}'),
+                    Text(
+                        'Wind speed:${_openWeatherController.weather.value?.wind?.speed}'),
+                    Text(
+                        'Rain 1H:${_openWeatherController.weather.value?.rain?.oneHour}'),
+                    Text(
+                        'Main temp:${_openWeatherController.weather.value?.main.temp}'),
+                  ],
+                ),
+              )
+            : const Center(child: CircularProgressIndicator()),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
