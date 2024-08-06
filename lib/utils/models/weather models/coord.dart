7c5ec13252decbafd93678f1b2994da193f7f3cb
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-06 06:20:02 +0530

    refactor: Update Coord.fromjson to handle int values for longitude and latitude

--- lib/utils/models/weather models/coord.dart
+++ lib/utils/models/weather models/coord.dart
@@ -1,3 +1,5 @@
+import 'package:skyscape/utils/parsers/parse_to_double.dart';
+
 class Coord {
   final double longitude;
   final double latitude;
@@ -5,6 +7,10 @@ class Coord {
   Coord({required this.longitude, required this.latitude});
 
   factory Coord.fromjson(Map<String, dynamic> json) {
-    return Coord(longitude: json['lon'], latitude: json['lat']);
+    //call parseValue function if int values
+    //are received instead of double (which is a rare case)
+    double lon = parseToDouble(json['lon']);
+    double lat = parseToDouble(json['lat']);
+    return Coord(longitude: lon, latitude: lat);
   }
 }
