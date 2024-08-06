53c93de91c4af18b2ef1c118a8756c55d23a5486
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-06 06:19:00 +0530

    refactor: Add parseToDouble function for converting int values to double

---
+++ lib/utils/parsers/parseToDouble.dart
@@ -0,0 +1,14 @@
+//Sometimes the json response received from OpenWeatherMap
+//contains values in int(int are obtained rarely)
+//instead of double(double are received most often)
+//To fix that problem parseValue function converts
+//int(obtained rarely) value to double
+double parseToDouble(dynamic value) {
+  if (value is int) {
+    return double.parse(value.toString());
+  } else if (value is double) {
+    return value;
+  } else {
+    throw Exception('Invalid Value Obtained');
+  }
+}
