37ce7f7f66d8df35000d970da147e934fd85a840
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-06 17:24:00 +0530

    refactor: Update code to improve performance and navigation using Get package

--- lib/pages/cities_list_page.dart
+++ lib/pages/cities_list_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:skyscape/controllers/cities_list_controller.dart';
 import 'package:skyscape/controllers/open_weather_controller.dart';
 import 'package:skyscape/routes/app_routes.dart';
 import 'package:skyscape/utils/measurements/ui_sizes.dart';
-import 'package:skyscape/utils/models/owm_city.dart';
-import 'package:skyscape/utils/repository/owm_cities_list_repo.dart';
 
 class CitiesListPage extends StatefulWidget {
   const CitiesListPage({super.key});
@@ -16,86 +15,87 @@ class CitiesListPage extends StatefulWidget {
 class _CitiesListPageState extends State<CitiesListPage> {
   final OpenWeatherController _openWeatherController =
       Get.find<OpenWeatherController>();
+  final CitiesListController _citiesListController =
+      Get.put(CitiesListController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cities List'),
       ),
-      body: FutureBuilder(
-        future: OwmCitiesListRepo().getOwmCitiesListData(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(
-              child: Text(
-                  "Error while fetching weather Data\n:${snapshot.error.toString()}"),
-            );
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            final List<OwmCity> owmCity = snapshot.data!;
-            return ListView.builder(
-              itemCount: owmCity.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  onLongPress: () async {
-                    return await showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text(owmCity[index].cityName),
-                          content: SizedBox(
-                            height: UiSizes().h20,
-                            child: SingleChildScrollView(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text('City ID: ${owmCity[index].cityId}'),
-                                  Text(
-                                      'Longitude: ${owmCity[index].longitude}'),
-                                  Text('Latitude: ${owmCity[index].latitude}'),
-                                  Text(
-                                      'Postal Code: ${owmCity[index].postalCode}'),
-                                  Text(
-                                      'Locality Long: ${owmCity[index].localityLong}'),
-                                  Text(
-                                      'Locality Short: ${owmCity[index].localityShort}'),
-                                  Text('District: ${owmCity[index].district}'),
-                                  Text('State: ${owmCity[index].state}'),
-                                  Text(
-                                      'State Short: ${owmCity[index].stateShort}'),
-                                  Text('Country: ${owmCity[index].country}'),
-                                  Text(
-                                      'Country Long: ${owmCity[index].countryLong}'),
-                                  Text(
-                                      'Country Short: ${owmCity[index].countryShort}'),
-                                ],
+      body: Obx(
+        () => _citiesListController.owmCitiesList.value != null
+            ? ListView.builder(
+                itemCount: _citiesListController.owmCitiesList.value?.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    onLongPress: () async {
+                      return await showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text(_citiesListController
+                                .owmCitiesList.value![index].cityName),
+                            content: SizedBox(
+                              height: UiSizes().h20,
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                        'City ID: ${_citiesListController.owmCitiesList.value?[index].cityId}'),
+                                    Text(
+                                        'Longitude: ${_citiesListController.owmCitiesList.value?[index].longitude}'),
+                                    Text(
+                                        'Latitude: ${_citiesListController.owmCitiesList.value?[index].latitude}'),
+                                    Text(
+                                        'Postal Code: ${_citiesListController.owmCitiesList.value?[index].postalCode}'),
+                                    Text(
+                                        'Locality Long: ${_citiesListController.owmCitiesList.value?[index].localityLong}'),
+                                    Text(
+                                        'Locality Short: ${_citiesListController.owmCitiesList.value?[index].localityShort}'),
+                                    Text(
+                                        'District: ${_citiesListController.owmCitiesList.value?[index].district}'),
+                                    Text(
+                                        'State: ${_citiesListController.owmCitiesList.value?[index].state}'),
+                                    Text(
+                                        'State Short: ${_citiesListController.owmCitiesList.value?[index].stateShort}'),
+                                    Text(
+                                        'Country: ${_citiesListController.owmCitiesList.value?[index].country}'),
+                                    Text(
+                                        'Country Long: ${_citiesListController.owmCitiesList.value?[index].countryLong}'),
+                                    Text(
+                                        'Country Short: ${_citiesListController.owmCitiesList.value?[index].countryShort}'),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
-                        );
-                      },
-                    );
-                  },
-                  onTap: () {
-                    _openWeatherController.getWeatherDataFromCoordnites(
-                        lat: double.parse(owmCity[index].latitude),
-                        lon: double.parse(owmCity[index].longitude));
-                    Get.offAndToNamed(AppRoutes.home);
-                  },
-                  title: Text(
-                    owmCity[index].cityName,
-                    style: const TextStyle(fontWeight: FontWeight.w500),
-                  ),
-                  subtitle: Text(
-                    '${owmCity[index].state}, ${owmCity[index].cityName}',
-                    style: const TextStyle(fontSize: 12),
-                  ),
-                );
-              },
-            );
-          }
-        },
+                          );
+                        },
+                      );
+                    },
+                    onTap: () {
+                      _openWeatherController.getWeatherDataFromCoordnites(
+                          lat: double.parse(_citiesListController
+                              .owmCitiesList.value![index].latitude),
+                          lon: double.parse(_citiesListController
+                              .owmCitiesList.value![index].longitude));
+                      Get.offAndToNamed(AppRoutes.home);
+                    },
+                    title: Text(
+                      '${_citiesListController.owmCitiesList.value?[index].cityName}',
+                      style: const TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    subtitle: Text(
+                      '${_citiesListController.owmCitiesList.value?[index].state}, ${_citiesListController.owmCitiesList.value?[index].cityName}',
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  );
+                },
+              )
+            : const Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
     );
   }
