c7962937722c6ae1c993586ad99b19dfd45792c8
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-07 16:18:05 +0530

    refactor: Remove defaultCity from SettingsController

--- lib/controllers/settings_controller.dart
+++ lib/controllers/settings_controller.dart
@@ -6,7 +6,6 @@ import 'package:skyscape/utils/enums/temperature_unit.dart';
 
 class SettingsController extends GetxController {
   late Rxn<ThemeMode> themeMode = Rxn<ThemeMode>();
-  late Rxn<String> defaultCity = Rxn<String>();
   late Rxn<TemperatureUnit> defaultUnit = Rxn<TemperatureUnit>();
 
   late SharedPreferences _prefs;
@@ -14,17 +13,18 @@ class SettingsController extends GetxController {
   @override
   void onInit() async {
     super.onInit();
-    if (await IsFirstRun.isFirstRun()) {
-      loadDefaults();
+    bool isFirstRun = await IsFirstRun.isFirstRun();
+    if (isFirstRun) {
+      debugPrint('Settings Controller: First Run Detected');
+      await loadDefaults();
     }
-    loadSettings();
+    await loadSettings();
   }
 
   Future<void> loadDefaults() async {
     _prefs = await SharedPreferences.getInstance();
     debugPrint("SettingsController.loadDefaults(): Loading Default Settings");
     _prefs.setInt('themeMode', ThemeMode.system.index);
-    _prefs.setString('defaultCity', 'Nagpur');
     _prefs.setInt('defaultUnit', TemperatureUnit.celsius.index);
   }
 
@@ -33,10 +33,9 @@ class SettingsController extends GetxController {
     _prefs.setInt('themeMode', ThemeMode.system.index);
     themeMode.value =
         ThemeMode.values.elementAt(_prefs.getInt('themeMode') ?? 0);
-    defaultCity.value = _prefs.getString('defaultCity');
     defaultUnit.value =
         TemperatureUnit.values.elementAt(_prefs.getInt('defaultUnit') ?? 0);
     debugPrint(
-        'Theme Mode: ${themeMode.value}, Default City: ${defaultCity.value}, Default Unit: ${defaultUnit.value}');
+        'Theme Mode: ${themeMode.value}, Default Unit: ${defaultUnit.value}');
   }
 }
