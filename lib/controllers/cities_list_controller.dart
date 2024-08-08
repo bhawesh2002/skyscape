920e246a2f169daa0a49a4171c9ab42f459a3e15
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-08 19:17:35 +0530

    refactor: Add search functionality to CitiesListController

--- lib/controllers/cities_list_controller.dart
+++ lib/controllers/cities_list_controller.dart
@@ -4,7 +4,7 @@ import 'package:skyscape/utils/models/owm_city.dart';
 import 'package:skyscape/utils/repository/owm_cities_list_repo.dart';
 
 class CitiesListController extends GetxController {
-  late Rxn<List<OwmCity>> owmCitiesList = Rxn<List<OwmCity>>();
+  late Rxn<List<OwmCity>> owmCitiesList = Rxn<List<OwmCity>>([]);
 
   @override
   void onInit() {
@@ -19,6 +19,17 @@ class CitiesListController extends GetxController {
       debugPrint('CitiesListController.getCitiesList(): Cities List Fetched');
     } catch (e) {
       debugPrint('CitiesListController.getCitiesList() error: $e');
+    }
+  }
+
+  List<OwmCity> searchCities({required String query}) {
+    if (owmCitiesList.value != null && query.isNotEmpty) {
+      List<OwmCity> searchedCities = owmCitiesList.value!.where((cities) {
+        return cities.cityName.toLowerCase().contains(query.toLowerCase());
+      }).toList();
+      return searchedCities;
+    } else {
+      return [];
     }
   }
 }
