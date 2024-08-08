42bd848e6f02d6383d1c628b1653b89361bed239
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-08 19:45:05 +0530

    refactor: Create aa search TextField  to search for Cities

--- lib/pages/cities_list_page.dart
+++ lib/pages/cities_list_page.dart
@@ -17,6 +17,7 @@ class _CitiesListPageState extends State<CitiesListPage> {
       Get.find<OpenWeatherController>();
   final CitiesListController _citiesListController =
       Get.find<CitiesListController>();
+  final TextEditingController _queryController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,83 +25,161 @@ class _CitiesListPageState extends State<CitiesListPage> {
         title: const Text('Cities List'),
       ),
       body: Obx(
-        () => _citiesListController.owmCitiesList.value != null
-            ? ListView.builder(
-                itemCount: _citiesListController.owmCitiesList.value?.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onLongPress: () async {
-                      return await showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: Text(_citiesListController
-                                .owmCitiesList.value![index].cityName),
-                            content: SizedBox(
-                              height: UiSizes().h20,
-                              child: SingleChildScrollView(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                        'City ID: ${_citiesListController.owmCitiesList.value?[index].cityId}'),
-                                    Text(
-                                        'Longitude: ${_citiesListController.owmCitiesList.value?[index].longitude}'),
-                                    Text(
-                                        'Latitude: ${_citiesListController.owmCitiesList.value?[index].latitude}'),
-                                    Text(
-                                        'Postal Code: ${_citiesListController.owmCitiesList.value?[index].postalCode}'),
-                                    Text(
-                                        'Locality Long: ${_citiesListController.owmCitiesList.value?[index].localityLong}'),
-                                    Text(
-                                        'Locality Short: ${_citiesListController.owmCitiesList.value?[index].localityShort}'),
-                                    Text(
-                                        'District: ${_citiesListController.owmCitiesList.value?[index].district}'),
-                                    Text(
-                                        'State: ${_citiesListController.owmCitiesList.value?[index].state}'),
-                                    Text(
-                                        'State Short: ${_citiesListController.owmCitiesList.value?[index].stateShort}'),
-                                    Text(
-                                        'Country: ${_citiesListController.owmCitiesList.value?[index].country}'),
-                                    Text(
-                                        'Country Long: ${_citiesListController.owmCitiesList.value?[index].countryLong}'),
-                                    Text(
-                                        'Country Short: ${_citiesListController.owmCitiesList.value?[index].countryShort}'),
-                                  ],
+        () => _citiesListController.allOwmCities.value != null
+            ? Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 8.0),
+                    child: TextField(
+                      controller: _queryController,
+                      onChanged: (value) {
+                        _citiesListController.searchCities(query: value);
+                      },
+                      decoration:
+                          const InputDecoration(hintText: "Enter City Name"),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: SizedBox(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 8),
+                        child: _citiesListController.searchedCities.isNotEmpty
+                            ? Text(
+                                '${_citiesListController.searchedCities.length} City(s) Found')
+                            : null,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      child: _citiesListController.searchedCities.isNotEmpty
+                          ? ListView.builder(
+                              itemCount:
+                                  _citiesListController.searchedCities.length,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  onLongPress: () async {
+                                    return await showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          title: Text(_citiesListController
+                                              .searchedCities[index].cityName),
+                                          content: SizedBox(
+                                            height: UiSizes().h20,
+                                            child: SingleChildScrollView(
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                      'City ID: ${_citiesListController.searchedCities[index].cityId}'),
+                                                  Text(
+                                                      'Longitude: ${_citiesListController.searchedCities[index].longitude}'),
+                                                  Text(
+                                                      'Latitude: ${_citiesListController.searchedCities[index].latitude}'),
+                                                  Text(
+                                                      'Postal Code: ${_citiesListController.searchedCities[index].postalCode}'),
+                                                  Text(
+                                                      'Locality Long: ${_citiesListController.searchedCities[index].localityLong}'),
+                                                  Text(
+                                                      'Locality Short: ${_citiesListController.searchedCities[index].localityShort}'),
+                                                  Text(
+                                                      'District: ${_citiesListController.searchedCities[index].district}'),
+                                                  Text(
+                                                      'State: ${_citiesListController.searchedCities[index].state}'),
+                                                  Text(
+                                                      'State Short: ${_citiesListController.searchedCities[index].stateShort}'),
+                                                  Text(
+                                                      'Country: ${_citiesListController.searchedCities[index].country}'),
+                                                  Text(
+                                                      'Country Long: ${_citiesListController.searchedCities[index].countryLong}'),
+                                                  Text(
+                                                      'Country Short: ${_citiesListController.searchedCities[index].countryShort}'),
+                                                ],
+                                              ),
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                    );
+                                  },
+                                  onTap: () {
+                                    _openWeatherController
+                                        .getWeatherDataFromCoordnites(
+                                            lat: double.parse(
+                                                _citiesListController
+                                                    .searchedCities[index]
+                                                    .latitude),
+                                            lon: double.parse(
+                                                _citiesListController
+                                                    .searchedCities[index]
+                                                    .longitude));
+                                    Get.offAndToNamed(AppRoutes.home);
+                                  },
+                                  title: Text(
+                                    _citiesListController
+                                        .searchedCities[index].cityName,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  subtitle: Text(
+                                    '${_citiesListController.searchedCities[index].state}, ${_citiesListController.searchedCities[index].cityName}',
+                                    style: const TextStyle(fontSize: 12),
+                                  ),
+                                );
+                              },
+                            )
+                          : Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                const SizedBox(
+                                  height: 56,
                                 ),
-                              ),
+                                Container(
+                                  padding: const EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Theme.of(context).brightness ==
+                                            Brightness.dark
+                                        ? Colors.white
+                                        : Colors.black,
+                                  ),
+                                  child: Icon(
+                                    _queryController.text.isNotEmpty
+                                        ? Icons.search_off
+                                        : Icons.search,
+                                    size: 48,
+                                    color: Theme.of(context).brightness ==
+                                            Brightness.dark
+                                        ? Colors.black
+                                        : Colors.white,
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 36,
+                                ),
+                                Text(
+                                  _queryController.text.isNotEmpty
+                                      ? "City Not Found!"
+                                      : "Search For Your City",
+                                  style: const TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )
+                              ],
                             ),
-                          );
-                        },
-                      );
-                    },
-                    onTap: () {
-                      _openWeatherController.getWeatherDataFromCoordnites(
-                          lat: double.parse(_citiesListController
-                              .owmCitiesList.value![index].latitude),
-                          lon: double.parse(_citiesListController
-                              .owmCitiesList.value![index].longitude));
-                      Get.offAndToNamed(AppRoutes.home);
-                    },
-                    title: Text(
-                      '${_citiesListController.owmCitiesList.value?[index].cityName}',
-                      style: const TextStyle(fontWeight: FontWeight.w500),
                     ),
-                    subtitle: Text(
-                      '${_citiesListController.owmCitiesList.value?[index].state}, ${_citiesListController.owmCitiesList.value?[index].cityName}',
-                      style: const TextStyle(fontSize: 12),
-                    ),
-                  );
-                },
+                  ),
+                ],
               )
             : const Center(
                 child: CircularProgressIndicator(),
               ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Obx(
-            () => Text('${_citiesListController.owmCitiesList.value?.length}')),
       ),
     );
   }
