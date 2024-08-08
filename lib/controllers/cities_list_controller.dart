5027961563cfc0ebace8744f53a10d486476b6b1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-08 19:44:27 +0530

    refactor: Update CitiesListController to use separate lists for allOwmCities and searchedCities

--- lib/controllers/cities_list_controller.dart
+++ lib/controllers/cities_list_controller.dart
@@ -4,7 +4,8 @@ import 'package:skyscape/utils/models/owm_city.dart';
 import 'package:skyscape/utils/repository/owm_cities_list_repo.dart';
 
 class CitiesListController extends GetxController {
-  late Rxn<List<OwmCity>> owmCitiesList = Rxn<List<OwmCity>>([]);
+  late Rxn<List<OwmCity>> allOwmCities = Rxn<List<OwmCity>>([]);
+  RxList<OwmCity> searchedCities = <OwmCity>[].obs;
 
   @override
   void onInit() {
@@ -15,21 +16,20 @@ class CitiesListController extends GetxController {
   Future<void> getCitiesList() async {
     try {
       debugPrint('CitiesListController.getCitiesList(): Fetching Cities List');
-      owmCitiesList.value = await OwmCitiesListRepo().getOwmCitiesListData();
+      allOwmCities.value = await OwmCitiesListRepo().getOwmCitiesListData();
       debugPrint('CitiesListController.getCitiesList(): Cities List Fetched');
     } catch (e) {
       debugPrint('CitiesListController.getCitiesList() error: $e');
     }
   }
 
-  List<OwmCity> searchCities({required String query}) {
-    if (owmCitiesList.value != null && query.isNotEmpty) {
-      List<OwmCity> searchedCities = owmCitiesList.value!.where((cities) {
+  void searchCities({required String query}) {
+    if (allOwmCities.value != null && query.isNotEmpty) {
+      searchedCities.value = allOwmCities.value!.where((cities) {
         return cities.cityName.toLowerCase().contains(query.toLowerCase());
       }).toList();
-      return searchedCities;
     } else {
-      return [];
+      searchedCities.value = [];
     }
   }
 }
