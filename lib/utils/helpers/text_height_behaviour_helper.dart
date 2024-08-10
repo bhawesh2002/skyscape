1c87b1022e750ab87a206bf466fc64a64989dbf0
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-10 06:39:43 +0530

    feat: Add gigaSansTextHeightBehaviour helper function

---
+++ lib/utils/helpers/text_height_behaviour_helper.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+TextHeightBehavior gigaSansTextHeightBehaviour() {
+  return const TextHeightBehavior(
+    applyHeightToFirstAscent: true,
+    applyHeightToLastDescent: false,
+  );
+}
