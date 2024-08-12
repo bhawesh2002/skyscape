b704f320a17a782ba2d4c19dccba0c11afcf15d0
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-12 12:08:56 +0530

    refactor: Remove unused FloatingActionButtonThemeData in AppTheme

--- lib/utils/theme/app_theme.dart
+++ lib/utils/theme/app_theme.dart
@@ -8,20 +8,12 @@ class AppTheme {
         colorScheme: const ColorScheme.light(
           primary: AppColors.purple,
         ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Color.fromARGB(255, 227, 199, 237),
-          foregroundColor: Colors.black,
-        ),
       );
   //DARK THEME
   static ThemeData darkTheme() => ThemeData(
         scaffoldBackgroundColor: const Color.fromARGB(255, 17, 17, 17),
         colorScheme: const ColorScheme.dark(
           primary: AppColors.purple,
-        ),
-        floatingActionButtonTheme: const FloatingActionButtonThemeData(
-          backgroundColor: Color.fromARGB(255, 227, 199, 237),
-          foregroundColor: Colors.black,
         ),
       );
 }
