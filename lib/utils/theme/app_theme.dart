e0206321115230bbe807298a4fa4a6be0520e831
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-12 11:56:02 +0530

    refactor: Update app theme colors

--- lib/utils/theme/app_theme.dart
+++ lib/utils/theme/app_theme.dart
@@ -4,11 +4,24 @@ import 'package:skyscape/utils/theme/app_colors.dart';
 class AppTheme {
   //LIGHT THEME
   static ThemeData lightTheme() => ThemeData(
-      scaffoldBackgroundColor: Colors.grey.shade100,
-      colorScheme: const ColorScheme.light(primary: AppColors.purple));
+        scaffoldBackgroundColor: Colors.grey.shade100,
+        colorScheme: const ColorScheme.light(
+          primary: AppColors.purple,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color.fromARGB(255, 227, 199, 237),
+          foregroundColor: Colors.black,
+        ),
+      );
   //DARK THEME
   static ThemeData darkTheme() => ThemeData(
         scaffoldBackgroundColor: const Color.fromARGB(255, 17, 17, 17),
-        colorScheme: const ColorScheme.dark(primary: AppColors.purple),
+        colorScheme: const ColorScheme.dark(
+          primary: AppColors.purple,
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color.fromARGB(255, 227, 199, 237),
+          foregroundColor: Colors.black,
+        ),
       );
 }
