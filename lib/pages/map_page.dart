a923c8786d7f16bb497814a6c8d56ff56f7e476c
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-13 18:07:10 +0530

    feat: Add MapPage to app navigation

---
+++ lib/pages/map_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:get/get.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:skyscape/controllers/location_controller.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final MapController _mapController = MapController();
+  final LocationController _locationController = Get.find<LocationController>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Map View"),
+      ),
+      body: FutureBuilder(
+        future: _locationController.getCurrentLocation(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else {
+            return Obx(() {
+              // Using Obx to reactively rebuild the map based on location changes
+              double latitude = _locationController.latitude.value;
+              double longitude = _locationController.longitude.value;
+
+              return Stack(
+                children: [
+                  FlutterMap(
+                    mapController: _mapController,
+                    options: MapOptions(
+                      initialCenter: LatLng(latitude, longitude),
+                      initialZoom: 15,
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            });
+          }
+        },
+      ),
+    );
+  }
+}
