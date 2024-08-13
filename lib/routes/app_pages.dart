aa6f4b11662b70a79fd8e1a42b8bbadd4e4ae46f
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-13 17:11:48 +0530

    feat: Add map page to app navigation

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 import 'package:skyscape/pages/cities_list_page.dart';
 import 'package:skyscape/pages/home.dart';
+import 'package:skyscape/pages/map_page.dart';
 import 'package:skyscape/routes/app_routes.dart';
 
 class AppPages {
   static final List<GetPage> pages = [
     GetPage(name: AppRoutes.home, page: () => const HomePage()),
     GetPage(name: AppRoutes.citiesList, page: () => const CitiesListPage()),
+    GetPage(name: AppRoutes.mapPage, page: () => const MapPage())
   ];
 }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,4 +1,5 @@
 class AppRoutes {
   static const String home = '/home';
   static const String citiesList = '/citiesList';
+  static const String mapPage = '/mapPage';
 }
