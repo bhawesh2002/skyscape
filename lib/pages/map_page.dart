140499cce1f5ab17cce8b01b1db98709b4009971
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2024-08-14 20:51:42 +0530

    refactor: Update MapPage to handle weather data and location changes

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:get/get.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:skyscape/controllers/location_controller.dart';
+import 'package:skyscape/controllers/open_weather_controller.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -14,6 +15,32 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   final MapController _mapController = MapController();
   final LocationController _locationController = Get.find<LocationController>();
+  final OpenWeatherController _openWeatherController =
+      Get.find<OpenWeatherController>();
+  late double? latitude;
+  late double? longitude;
+
+  @override
+  void initState() {
+    if (_openWeatherController.weather.value?.coord.latitude != null &&
+        _openWeatherController.weather.value?.coord.latitude != null) {
+      latitude = _openWeatherController.weather.value?.coord.latitude;
+      longitude = _openWeatherController.weather.value?.coord.longitude;
+    } else {
+      getLocation();
+    }
+
+    super.initState();
+  }
+
+  Future<void> getLocation() async {
+    await _locationController.getCurrentLocation().then((_) {
+      setState(() {
+        latitude = _locationController.latitude.value;
+        longitude = _locationController.longitude.value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,43 +48,26 @@ class _MapPageState extends State<MapPage> {
       appBar: AppBar(
         title: const Text("Map View"),
       ),
-      body: FutureBuilder(
-        future: _locationController.getCurrentLocation(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          } else {
-            return Obx(() {
-              // Using Obx to reactively rebuild the map based on location changes
-              double latitude = _locationController.latitude.value;
-              double longitude = _locationController.longitude.value;
-
-              return Stack(
-                children: [
-                  FlutterMap(
+      body: Stack(
+        children: [
+          Center(
+            child: (latitude != null && longitude != null)
+                ? FlutterMap(
                     mapController: _mapController,
                     options: MapOptions(
-                      initialCenter: LatLng(latitude, longitude),
-                      initialZoom: 14,
+                      initialCenter: LatLng(latitude!, longitude!),
+                      initialZoom: 12,
                     ),
                     children: [
                       TileLayer(
                         urlTemplate:
                             'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                      ),
+                      )
                     ],
-                  ),
-                ],
-              );
-            });
-          }
-        },
+                  )
+                : const CircularProgressIndicator(),
+          ),
+        ],
       ),
     );
   }
